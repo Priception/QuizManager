091d8c5be00ef99632532d8be635ced34ca3d280
Priception YT <priception@aol.co.uk>
2022-08-05 00:01:13 +0100

    Fixed issues with Navigation Menu

---
+++ QuizManager/Pages/CreateQuiz.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuizManager.Pages
+{
+    public class QuizManagerModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public QuizManagerModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
