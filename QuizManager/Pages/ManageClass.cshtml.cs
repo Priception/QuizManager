f2e8e928bc93ba12ab3a2ddda46adc20f5357562
Priception YT <priception@aol.co.uk>
2022-08-06 01:10:11 +0100

    Added Settings Page and Manage Class page and fixed sclaing issues on other pages

---
+++ QuizManager/Pages/ManageClass.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuizManager.Pages
+{
+    public class ManageClassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
